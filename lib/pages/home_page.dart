5e5acc522bfb0ce8ebe295174ec68c5c06640916
Ahmad Dzaky Abrori <broridzaky2702@gmail.com>
2021-05-15 15:58:02 +0700

    User Profile.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,39 @@
+import 'package:chatty_apps/theme.dart';
 import 'package:flutter/material.dart';
+import '';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Home Page'),
+      backgroundColor: primaryColor,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 40,
+              ),
+              Image.asset(
+                'assets/images/profile_pic.png',
+                height: 100,
+                width: 100,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Sabrina Carpenter',
+                style: userNameTextStyle,
+              ),
+              Text(
+                'Travel Freelancer',
+                style: userBioTextStyle,
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
