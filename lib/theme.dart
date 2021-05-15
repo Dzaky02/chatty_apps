c6e4dc9f89ba7a78cf70d2af572a4263b89b7326
Ahmad Dzaky Abrori <broridzaky2702@gmail.com>
2021-05-15 15:43:48 +0700

    Theme Style.

---
+++ lib/theme.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+// Color theme
+Color primaryColor = Color(0XFF1F8DF5);
+Color backgroundChatColor = Color(0XFFF8FAFC);
+Color whiteColor = Color(0XFFFFFFFF);
+Color lightBlueColor = Color(0XFF9ED0FF);
+Color darkBlueColor = Color(0XFF2C3A59);
+Color greyColor = Color(0XFFEBEFF3);
+Color darkGreyColor = Color(0XFF505C6B);
+Color lightGreyColor = Color(0XFF808BA2);
+Color greenColor = Color(0XFF29CB9E);
+
+// TextStyle theme
+TextStyle userNameTextStyle = TextStyle(
+  color: whiteColor,
+  fontWeight: FontWeight.w400,
+  fontSize: 20,
+);
+
+TextStyle userBioTextStyle = TextStyle(
+  color: lightBlueColor,
+  fontWeight: FontWeight.w300,
+  fontSize: 16,
+);
+
+TextStyle titleTextStyle = TextStyle(
+  color: darkBlueColor,
+  fontWeight: FontWeight.w500,
+  fontSize: 16,
+);
+
+TextStyle subTitleTextStyle = TextStyle(
+  color: lightGreyColor,
+  fontWeight: FontWeight.w300,
+  fontSize: 14,
+);
+
+TextStyle unReadTextStyle = TextStyle(
+  color: darkBlueColor,
+  fontWeight: FontWeight.w400,
+  fontSize: 14,
+);
+
+TextStyle chatMessageTextStyle = TextStyle(
+  color: darkGreyColor,
+  fontWeight: FontWeight.w400,
+  fontSize: 16,
+);
+
+TextStyle chatTimeTextStyle = TextStyle(
+  color: darkGreyColor,
+  fontWeight: FontWeight.w300,
+  fontSize: 14,
+);
